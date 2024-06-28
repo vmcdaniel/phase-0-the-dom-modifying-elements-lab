77c9d5b1421f61fd582f671910fd81e039898fcf
Victoria <vmcdaniel459@gmail.com>
2024-06-27 20:02:38 -0400

    good job

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.querySelector('main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Victoria is the champion';
+append(document.mainElement, newHeader);
